34a0b3976aaceae6424cb9e145e75b20cf82b868
Phạm Trần Hoàng Vũ <hoangvucntt95@gmail.com>
2017-06-14 15:25:11 +0700

    Xu ly Tap Tin

---
+++ InternalStorage/src/com/example/internalstorage/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.internalstorage;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends Activity {
+
+	EditText editdata;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        editdata= (EditText) findViewById(R.id.editdata);
+    	
+    }
+    public void btnreaddata(View v)
+    {
+    	String data;
+    	 InputStream in= getResources().openRawResource(R.drawable.myfile);
+    	 InputStreamReader inreader=new InputStreamReader(in);
+    	 BufferedReader bufreader=new BufferedReader(inreader);
+    	 StringBuilder builder=new StringBuilder();
+    	 if(in!=null)
+    	 {
+	    	 try
+	    	{
+		    	 while((data=bufreader.readLine())!=null)
+		    	 {
+		    	 builder.append(data);
+		    	 builder.append("\n");
+		    	 }
+		    	 in.close();
+				editdata.setText(builder.toString());
+	    	}
+		    	 catch(IOException ex){
+		    	 Log.e("ERROR", ex.getMessage());
+	    	 }
+    	}
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
+}
